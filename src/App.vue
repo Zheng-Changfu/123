358ddbd1820141f66bf2e9ec54446f719d13a292
Zheng-Changfu <zcfpromise@163.com>
2025-06-05 15:14:26 +0800

    feat: 1

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref, computed } from 'vue'
+import { get } from 'lodash-es'
+
+let id = 0
+const values = ref({
+  c: [
+    { a: 1 }
+  ]
+})
+
+const values2 = computed(() => {
+  /**
+   * add 
+   *  1,2
+   * 
+   * add 
+   *  1,2,3
+   *  3,4,5
+   *  4,5,6
+   */
+  return values.value.c.map(item => ({
+    ...item,
+    id: ++id
+  }))
+})
+
+
+function add() {
+  values.value.c.push({ a: 2 })
+}
+
+
+</script>
+<template>
+  <button @click="add">add</button>
+  <button @click="() => console.log(values2)">log</button>
+  <div v-for="item in values2">{{ item.a }}</div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import 'virtual:uno.css'
+import App from './App.vue'
+import Naive from 'naive-ui'
+createApp(App).use(Naive).mount('#app')
